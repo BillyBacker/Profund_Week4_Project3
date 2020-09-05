14330ed0ec37df0d5feb704d25b4739296b4a02b
BillyBacker <38096723+BillyBacker@users.noreply.github.com>
2020-09-05 13:14:13 +0700

    Add project files.

---
+++ Profund_Week4_Project33/Source.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+	int r;
+	scanf("%d", &r);
+	r -= 1;
+	int thickness = r / 1.5;
+	for (int row = 0; row < 2 * r+1; row++) {
+		for (int col = 0; col < 2 * r+1; col++) {
+			if (pow((col - r), 2) + pow((row - r), 2) <= pow(r, 2) && (pow((col - r), 2) + pow((row - r), 2) > pow((r - thickness), 2))) {
+				printf("* ");
+			}
+			else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+}
